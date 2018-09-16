b10bdf3fe5f933a8d0aa374b9fa823caa0b5577f
Md. Ayub Khan <aiubkhan95@gmail.com>
2018-09-16 15:32:27 +0600

    Structure of the Application

--- app.js
+++ app.js
@@ -6,8 +6,21 @@ var budgetController = (function() {
 
   return {
     publicTest: function(b) {
-      console.log(add(b))
+      console.log(add(b));
     }
   };
 
 })();
+
+var UIController = (function() {
+  
+
+})();
+
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+  
+
+
+})(budgetController, UIController);
