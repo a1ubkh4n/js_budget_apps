415d78dc7c0a0623399136839822ff01f25ff8fa
Md. Ayub Khan <aiubkhan95@gmail.com>
2018-09-16 23:47:06 +0600

    adding new Item to budget Controller

--- app.js
+++ app.js
@@ -1,9 +1,59 @@
-
-
 // BUDGET CONTROLLER
 var budgetController = (function() {
   
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
 
+  var data = {
+    allItems:{
+      expense: [],
+      income: []
+    },
+
+    totals: {
+      expense: 0,
+      income: 0
+    }
+  };
+
+  return {
+    addItem : function(type, des, val) {
+      var newItem, ID;
+
+      // Create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // create new item based on 'income' or 'expense'
+      if (type === 'expense') {
+        newItem = new Expense(ID, des, val);
+      } else if(type === 'income') {
+        newItem = new Income(ID, des, val);
+      }
+
+      // Push it into data structure
+      data.allItems[type].push(newItem);
+
+      // Return the new element
+      return newItem;
+    },
+
+    testing : function() {
+      console.log(data);
+    }
+  };
 
 })();
 
@@ -52,11 +102,14 @@ var controller = (function(budgetCtrl, UICtrl) {
   };
 
   var controlAddItem = function() {
+    var input, newItem;
+
   // Get the filled input data
-    var input = UICtrl.getInput();
+    input = UICtrl.getInput();
 
    // Add the item to the budget controller
-    
+    newItem = budgetController.addItem(input.type, input.description, input.value);
+
   // Add the item to the UI
 
   // Calculate the budget 
