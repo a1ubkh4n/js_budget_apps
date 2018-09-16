09368fa551f110987a0e255c2893a3dc74e75933
Md. Ayub Khan <aiubkhan95@gmail.com>
2018-09-16 15:57:56 +0600

    Add new item function  created

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
-  var x = 23;
-  var add = function(a) {
-    return x + a;
-  };
+  
 
-  return {
-    publicTest: function(b) {
-      console.log(add(b));
-    }
-  };
 
 })();
 
+// UI CONTROLLER
 var UIController = (function() {
   
 
 })();
 
-
+// GLOBAL APPLICATION CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-  
+  var addButton = document.querySelector('.add-btn');
 
+  var controlAddItem = function() {
+  // Get the filled input data
+
+   // Add the item to the budget controller
+
+  // Add the item to the UI
+
+  // Calculate the budget 
+
+  // display the budget on the UI
+  };
+
+  addButton.addEventListener('click', controlAddItem);
+  document.addEventListener('keypress', controlAddItem);
+
+  function keyPressEventHadler(event) {
+    if(event.keyCode === 13 || event.which === 13) {
+      controlAddItem();
+    }
+  }
+  
 
 })(budgetController, UIController);
