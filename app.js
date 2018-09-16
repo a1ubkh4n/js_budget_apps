dbeb132c8c82c6e6c6d1e3e4d070efbffa393602
Md. Ayub Khan <aiubkhan95@gmail.com>
2018-09-16 16:20:48 +0600

    manipulate data from one to another controller and get item from UI

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 // BUDGET CONTROLLER
 var budgetController = (function() {
   
@@ -7,18 +9,40 @@ var budgetController = (function() {
 
 // UI CONTROLLER
 var UIController = (function() {
+
+  var DOMString = {
+    inputType : '.add-type',
+    inputDescription : '.add-description',
+    inputValue : '.add-value',
+    inputButton : '.add-btn'
+  };
   
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMString.inputType).value, // will be either income or expenses
+        description: document.querySelector(DOMString.inputDescription).value,
+       value: document.querySelector(DOMString.inputValue).value
+      };     
+    },
+    getDOMStrings: function() {
+      return DOMString;
+    }
+  };
 
 })();
 
 // GLOBAL APPLICATION CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-  var addButton = document.querySelector('.add-btn');
+  var DOM = UICtrl.getDOMStrings();
+
+  var addButton = DOM.inputButton;
 
   var controlAddItem = function() {
   // Get the filled input data
-
+    var input = UICtrl.getInput();
+    alert(input);
    // Add the item to the budget controller
 
   // Add the item to the UI
@@ -29,9 +53,9 @@ var controller = (function(budgetCtrl, UICtrl) {
   };
 
   addButton.addEventListener('click', controlAddItem);
-  document.addEventListener('keypress', controlAddItem);
+  document.addEventListener('keypress', keyPressEventHandler);
 
-  function keyPressEventHadler(event) {
+  function keyPressEventHandler(event) {
     if(event.keyCode === 13 || event.which === 13) {
       controlAddItem();
     }
